e3962904f05b189ec0c74d317947e5e4e0367960
INGENIO <ingeniokhalif@gmail.com>
2023-06-19 11:59:39 +0100

    fix admin's orders listing

--- controllers/cart.js
+++ controllers/cart.js
@@ -15,6 +15,8 @@ const addToCart = (req, res) => {
 
   cart[productId] = { name: name, quantity: quantity, price: price };
 
+  console.log(cart);
+
   req.session.cart = cart;
 
   const count = Object.keys(cart).length;
