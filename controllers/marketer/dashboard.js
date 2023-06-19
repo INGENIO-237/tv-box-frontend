c5f1425ca7fbb135a2c010070855d1eb9d41ccc8
INGENIO <ingeniokhalif@gmail.com>
2023-06-19 12:59:33 +0100

    fix marketer's dashboard

--- controllers/marketer/dashboard.js
+++ controllers/marketer/dashboard.js
@@ -1,10 +1,25 @@
 const axios = require("axios");
+const formatDate = require("../../utils/format-date");
+require("dotenv").config();
 
+const dashboardMarketer = (req, res) => {
+  axios
+    .get(process.env.BACKEND_ENDPOINT + "/users/promos/gains")
+    .then((response) => {
+      const gains = response.data;
 
-const dashboardMarketer = (req, res) =>{
-    res.render("pages/marketer/dashboard-com", {
-        layout: "dashboard-layout.ejs",
+      gains.forEach((gain) => {
+        gain.date_gain = formatDate(gain.date_gain);
       });
-}
+
+      res.render("pages/marketer/dashboard-com", {
+        layout: "dashboard-layout.ejs",
+        gains,
+      });
+    })
+    .catch((error) => {
+      console.error(error.response.data.message);
+    });
+};
 
 module.exports = dashboardMarketer;
