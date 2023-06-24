68b36b130495136b658be1b74f599fecc303a3d2
INGENIO <ingeniokhalif@gmail.com>
2023-06-24 18:12:33 +0100

    fix image not displaying after being uploaded from a mobile phone

--- utils/image-path.js
+++ utils/image-path.js
@@ -1,9 +1,11 @@
 const imagePath = (product) => {
-  const splittedImagePath = product.image_art.split("\\");
-  const slicedImagePath = splittedImagePath.slice(1);
-  const joinedImagePath = slicedImagePath.join("/");
+  if (product.image_art == "\\") {
+    const splittedImagePath = product.image_art.split("\\");
+    const slicedImagePath = splittedImagePath.slice(1);
+    const joinedImagePath = slicedImagePath.join("/");
 
-  return joinedImagePath;
+    return joinedImagePath;
+  }
 };
 
 module.exports = { imagePath };
